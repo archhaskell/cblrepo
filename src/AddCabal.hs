2ca7f1562a96be50f1d2d92a4509c7309956c386
Magnus Therning <magnus@therning.org>
2011-03-06 18:39:25 +0000

    Moving out function for reading cabal file, and adding a patchdir argument.

--- src/AddCabal.hs
+++ src/AddCabal.hs
@@ -35,81 +35,12 @@ addCabal = do
     db <- liftIO $ readDb dbFn
     cbls <- cfgGet cbls
     dR <- cfgGet dryRun
-    genPkgs <- liftIO $ mapM (\ c -> withTemporaryDirectory "/tmp/cblrepo." (readCabal c)) cbls
+    genPkgs <- liftIO $ mapM (\ c -> withTemporaryDirectory "/tmp/cblrepo." (readCabal "patches" c)) cbls
     let pkgNames = map ((\ (P.PackageName n) -> n ) . P.pkgName . package . packageDescription) genPkgs
     let tmpDb = filter (\ p -> not $ pkgName p `elem` pkgNames) db
     case doAddCabal tmpDb genPkgs of
         Left (unSats, brksOthrs) -> liftIO (mapM_ printUnSat unSats >> mapM_ printBrksOth brksOthrs)
         Right newDb -> liftIO $ unless dR $ saveDb newDb dbFn
-
-data LocType = Url | Idx | File
-
-readCabal loc tmpDir = let
-        locType
-            | isInfixOf "://" loc = Url
-            | ',' `elem` loc = Idx
-            | otherwise = File
-
-        copyCabal tmpDir loc = copyFile loc fn >> return fn
-            where fn = tmpDir </> takeFileName loc
-
-        downloadCabal tmpDir loc = getFromURL loc fn >> return fn
-            where
-                fn = tmpDir </> takeFileName loc
-
-        extractCabal tmpDir loc = let
-                (p, (_: v)) = span (/= ',') loc
-                path = p </> v </> p ++ ".cabal"
-                pkgStr = p ++ " " ++ v
-                fn = tmpDir </> (p ++ ".cabal")
-
-                esFindEntry p (Next e es) = if p == (entryPath e)
-                    then Just e
-                    else esFindEntry p es
-                esFindEntry _ _ = Nothing
-
-                eGetContent e = let
-                        ec = entryContent e
-                    in case ec of
-                        NormalFile c _ -> Just $ BS.unpack c
-                        _ -> Nothing
-
-            in do
-                fp <- getAppUserDataDirectory "cblrepo"
-                es <- liftM (Tar.read . GZip.decompress)
-                    (BS.readFile $ fp </> "00-index.tar.gz")
-                e <- maybe (error $ "No entry for " ++ pkgStr)
-                    return
-                    (esFindEntry path es)
-                cbl <- maybe (error $ "Failed to extract contents for " ++ pkgStr)
-                    return
-                    (eGetContent e)
-                writeFile fn cbl
-                return fn
-
-        extractName fn = liftM name $ readPackageDescription silent fn
-            where
-                packageName (P.PackageName s) = s
-                name = packageName . P.pkgName . package . packageDescription
-
-        applyPatch oFn pFn = do
-            (ec, _, err) <- readProcessWithExitCode "patch" [oFn, pFn] ""
-            case ec of
-                ExitSuccess -> return ()
-                ExitFailure _ -> hPutStrLn stderr "Failed patching the .cabal file" >> exitFailure
-
-    in do
-        fn <- case locType of
-            File -> copyCabal tmpDir loc
-            Idx -> extractCabal tmpDir loc
-            Url -> downloadCabal tmpDir loc
-        pn <- extractName fn
-        let patchName = "patch.cabal." ++ pn
-        doPatch <- fileExist patchName
-        if doPatch
-            then applyPatch fn patchName
-            else return ()
-        readPackageDescription silent fn
 
 doAddCabal db pkgs = let
         (succs, fails) = partition (canBeAdded db) pkgs
--- src/Utils.hs
+++ src/Utils.hs
@@ -1,18 +1,28 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 module Utils where
 
+import Codec.Archive.Tar as Tar
+import Codec.Compression.GZip as GZip
 import Control.Concurrent
 import Control.Monad
 import Control.Monad.Error
 import Control.Monad.Reader
 import Data.Data
+import Data.List
 import Data.Typeable
 import Distribution.Package
+import Distribution.PackageDescription
+import Distribution.PackageDescription.Parse
 import Distribution.Text
+import Distribution.Verbosity
+import System.Directory
 import System.Exit
+import System.FilePath
 import System.IO
+import System.Posix.Files
 import System.Process
 import qualified Control.Exception as CE
+import qualified Data.ByteString.Lazy.Char8 as BS
 
 -- {{{1 Dependency
 depName (Dependency (PackageName n) _) = n
@@ -55,3 +65,73 @@ getFromURL url fn = do
             hPutStrLn stderr ("Failed downloading " ++ url)
             hPutStrLn stderr er
             exitFailure
+
+-- {{{1 manipulations of Cabal files
+data LocType = Url | Idx | File
+
+-- | Read in a Cabal file.
+readCabal patchDir loc tmpDir = let
+        locType
+            | isInfixOf "://" loc = Url
+            | ',' `elem` loc = Idx
+            | otherwise = File
+
+        copyCabal tmpDir loc = copyFile loc fn >> return fn
+            where fn = tmpDir </> takeFileName loc
+
+        downloadCabal tmpDir loc = getFromURL loc fn >> return fn
+            where
+                fn = tmpDir </> takeFileName loc
+
+        extractCabal tmpDir loc = let
+                (p, (_: v)) = span (/= ',') loc
+                path = p </> v </> p ++ ".cabal"
+                pkgStr = p ++ " " ++ v
+                fn = tmpDir </> (p ++ ".cabal")
+
+                esFindEntry p (Next e es) = if p == (entryPath e)
+                    then Just e
+                    else esFindEntry p es
+                esFindEntry _ _ = Nothing
+
+                eGetContent e = let
+                        ec = entryContent e
+                    in case ec of
+                        NormalFile c _ -> Just $ BS.unpack c
+                        _ -> Nothing
+
+            in do
+                fp <- getAppUserDataDirectory "cblrepo"
+                es <- liftM (Tar.read . GZip.decompress)
+                    (BS.readFile $ fp </> "00-index.tar.gz")
+                e <- maybe (error $ "No entry for " ++ pkgStr)
+                    return
+                    (esFindEntry path es)
+                cbl <- maybe (error $ "Failed to extract contents for " ++ pkgStr)
+                    return
+                    (eGetContent e)
+                writeFile fn cbl
+                return fn
+
+        extractName fn = liftM name $ readPackageDescription silent fn
+            where
+                packageName (PackageName s) = s
+                name = packageName . pkgName . package . packageDescription
+
+        applyPatch oFn pFn = do
+            (ec, _, err) <- readProcessWithExitCode "patch" [oFn, pFn] ""
+            case ec of
+                ExitSuccess -> return ()
+                ExitFailure _ ->
+                    hPutStrLn stderr "Failed patching the .cabal file" >> exitFailure
+
+    in do
+        cblFn <- case locType of
+            File -> copyCabal tmpDir loc
+            Idx -> extractCabal tmpDir loc
+            Url -> downloadCabal tmpDir loc
+        pn <- extractName cblFn
+        let patchFn = patchDir </> ("patch.cabal." ++ pn)
+        return patchFn
+        fileExist patchFn >>= flip when (applyPatch cblFn patchFn)
+        readPackageDescription silent cblFn
