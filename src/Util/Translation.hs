de9df07d398d70255c852f4de53e3d14c4eeb0b4
Magnus Therning <magnus@therning.org>
2013-11-01 00:05:49 +0100

    Make exes in lib packages use dynamic linking.
    Signed-off-by: Magnus Therning <magnus@therning.org>

--- src/Util/Translation.hs
+++ src/Util/Translation.hs
@@ -180,7 +180,8 @@ instance Pretty ArchPkg where
                         maybe empty (\ _ ->
                             text $ "patch -p4 < \"${srcdir}/source.patch\"")
                             buildPatchFile <$>
-                        nest 4 (text "runhaskell Setup configure -O -p --enable-split-objs --enable-shared \\" <$>
+                        nest 4 (text "runhaskell Setup configure -O -p \\" <$>
+                            text "--enable-split-objs --enable-shared --enable-executable-dynamic \\" <$>
                             text "--prefix=/usr --docdir=\"/usr/share/doc/${pkgname}\" \\" <$>
                             text "--libsubdir=\\$compiler/site-local/\\$pkgid" <> confFlags) <$>
                         text "runhaskell Setup build" <$>
