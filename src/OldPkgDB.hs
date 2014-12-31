7fed0461030be7afd01af5a00c3571a11669d237
Magnus Therning <magnus@therning.org>
2014-12-31 07:10:10 +0100

    Prepare for changes to DB format.
    Replace OldPkgDB with a copy of PkgDB.  This also removes all use of the json
    package, so the dependency on it can be removed.
    
    Signed-off-by: Magnus Therning <magnus@therning.org>

--- src/OldPkgDB.hs
+++ src/OldPkgDB.hs
@@ -1,5 +1,5 @@
 {-
- - Copyright 2011 Per Magnus Therning
+ - Copyright 2011-2013 Per Magnus Therning
  -
  - Licensed under the Apache License, Version 2.0 (the "License");
  - you may not use this file except in compliance with the License.
@@ -14,21 +14,24 @@
  - limitations under the License.
  -}
 
+{-# LANGUAGE TemplateHaskell #-}
+
 module OldPkgDB where
 
 -- {{{1 imports
+import Control.Arrow
 import Control.Exception as CE
 import Control.Monad
-import Data.Data
 import Data.List
 import Data.Maybe
-import Data.Typeable
 import Distribution.PackageDescription
-import Distribution.Text
 import System.IO.Error
-import Text.JSON
 import qualified Distribution.Package as P
 import qualified Distribution.Version as V
+
+import Data.Aeson (decode, encode)
+import Data.Aeson.TH (deriveJSON, defaultOptions, Options(..), SumEncoding(..))
+import qualified Data.ByteString.Lazy.Char8 as C
 
 -- {{{ temporary
 _depName (P.Dependency (P.PackageName n) _) = n
@@ -123,7 +126,7 @@ addPkg :: CblDB -> String -> Pkg -> CblDB
 addPkg db n p = nubBy cmp newdb
     where
         cmp (CP n1 _) (CP n2 _) = n1 == n2
-        newdb = (CP n p):db
+        newdb = CP n p:db
 
 addPkg2 :: CblDB -> CblPkg -> CblDB
 addPkg2 db (CP n p) = addPkg db n p
@@ -141,7 +144,7 @@ bumpRelease :: CblDB -> String -> CblDB
 bumpRelease db n = let
         doBump (CP n' p@RepoPkg { release = r }) = CP n' (p { release = nr })
             where
-                nr = show $ (read r) + 1
+                nr = show $ read r + (1 :: Int)
         doBump p = p
     in maybe db (addPkg2 db . doBump) (lookupPkg db n)
 
@@ -151,12 +154,12 @@ lookupPkg (p:db) n
     | n == pkgName p = Just p
     | otherwise = lookupPkg db n
 
-lookupDependants db n = filter (/= n) $ map pkgName $ filter (\ p -> doesDependOn p n) db
+lookupDependants db n = filter (/= n) $ map pkgName $ filter (`doesDependOn` n) db
     where
-        doesDependOn p n = n `elem` (map _depName $ pkgDeps p)
+        doesDependOn p n = n `elem` map _depName (pkgDeps p)
 
 transitiveDependants :: CblDB -> [String] -> [String]
-transitiveDependants db names = keepLast $ concat $ map transUsersOfOne names
+transitiveDependants db names = keepLast $ concatMap transUsersOfOne names
     where
         transUsersOfOne n = n : transitiveDependants db (lookupDependants db n)
         keepLast = reverse . nub . reverse
@@ -164,80 +167,33 @@ transitiveDependants db names = keepLast $ concat $ map transUsersOfOne names
 -- Todo: test
 checkDependants :: CblDB -> String -> V.Version -> [(String, Maybe P.Dependency)]
 checkDependants db n v = let
-        d1 = catMaybes $ map (lookupPkg db) (lookupDependants db n)
-        d2 = map (\ p -> (pkgName p, getDependencyOn n p)) d1
+        d1 = mapMaybe (lookupPkg db) (lookupDependants db n)
+        -- d2 = map (\ p -> (pkgName p, getDependencyOn n p)) d1
+        d2 = map (pkgName &&& getDependencyOn n ) d1
         fails = filter (not . V.withinRange v . _depVersionRange . fromJust . snd) d2
     in fails
 
 readDb :: FilePath -> IO CblDB
-readDb fp = (flip CE.catch)
+readDb fp = handle
     (\ e -> if isDoesNotExistError e
         then return emptyPkgDB
         else throwIO e)
     $ do
-        r <- readFile fp >>= return . sequence . map decode . lines
+        r <- (mapM decode . C.lines) `liftM` C.readFile fp
         case r of
-            Ok a -> return a
-            Error s -> fail s
+            Just a -> return a
+            Nothing -> fail "JSON parsing failed"
 
 saveDb :: CblDB -> FilePath -> IO ()
-saveDb db fp = writeFile fp s
+saveDb db fp = C.writeFile fp s
     where
-        s = unlines $ map encode $ sort db
+        s = C.unlines $ map encode $ sort db
 
 -- {{{1 JSON instances
-instance JSON CblPkg where
-    showJSON (CP n p) = showJSON (n, p)
-
-    readJSON object = do
-        (n,p) <- readJSON object
-        return (CP n p)
-
-instance JSON V.Version where
-    showJSON v = makeObj [ ("Version", showJSON $ display v) ]
-
-    readJSON object = do
-        obj <- readJSON object
-        version <- valFromObj "Version" obj
-        maybe (fail "Not a Version object") return (simpleParse version)
-
-instance JSON P.Dependency where
-    showJSON d = makeObj [ ("Dependency", showJSON $ display d) ]
-
-    readJSON object = do
-        obj <- readJSON object
-        dep <- valFromObj "Dependency" obj
-        maybe (fail "Not a Dependency object") return (simpleParse dep)
-
-instance JSON FlagName where
-    showJSON (FlagName n) = makeObj [ ("FlagName", showJSON n) ]
-
-    readJSON object = do
-        obj <- readJSON object
-        n <- valFromObj "FlagName" obj
-        return $ FlagName n
-
-instance JSON Pkg where
-    showJSON p@(GhcPkg { version = v}) = makeObj [("GhcPkg", showJSON v)]
-    showJSON p@(DistroPkg { version = v, release = r}) =
-        makeObj [("DistroPkg", showJSON (v, r))]
-    showJSON p@(RepoPkg { version = v, deps = d, flags = fa, release = r }) =
-        makeObj [("RepoPkg", showJSON (v, d, fa, r))]
-
-    readJSON object = let
-            readGhc = do
-                obj <- readJSON object
-                v <- valFromObj "GhcPkg" obj >>= readJSON
-                return $ GhcPkg v
-
-            readDistro = do
-                obj <- readJSON object
-                (v, r) <- valFromObj "DistroPkg" obj >>= readJSON
-                return $ DistroPkg v r
-
-            readRepo = do
-                obj <- readJSON object
-                (v, d, fa, r) <- valFromObj "RepoPkg" obj >>= readJSON
-                return $ RepoPkg v d fa r
-
-        in readGhc `mplus` readDistro `mplus` readRepo `mplus` fail "Not a Pkg object"
+$(deriveJSON defaultOptions { sumEncoding = ObjectWithSingleField, allNullaryToStringTag = False } ''V.Version)
+$(deriveJSON defaultOptions { sumEncoding = ObjectWithSingleField, allNullaryToStringTag = False } ''V.VersionRange)
+$(deriveJSON defaultOptions { sumEncoding = ObjectWithSingleField, allNullaryToStringTag = False } ''P.Dependency)
+$(deriveJSON defaultOptions { sumEncoding = ObjectWithSingleField, allNullaryToStringTag = False } ''P.PackageName)
+$(deriveJSON defaultOptions { sumEncoding = ObjectWithSingleField, allNullaryToStringTag = False } ''FlagName)
+$(deriveJSON defaultOptions { sumEncoding = ObjectWithSingleField, allNullaryToStringTag = False } ''Pkg)
+$(deriveJSON defaultOptions { sumEncoding = ObjectWithSingleField, allNullaryToStringTag = False } ''CblPkg)
