a9dfafbdf1cec162998a803968a2a4a2b86c792b
Magnus Therning <magnus@therning.org>
2016-03-14 09:27:53 +0100

    Bump the GHC release to 3.
    Signed-off-by: Magnus Therning <magnus@therning.org>

--- src/Util/Misc.hs
+++ src/Util/Misc.hs
@@ -57,7 +57,7 @@ progName = "cblrepo"
 dbName = progName ++ ".db"
 
 ghcDefVersion = Version [7, 10, 3] []
-ghcDefRelease = 2 :: Int
+ghcDefRelease = 3 :: Int
 ghcVersionDep :: Version -> Int -> String
 ghcVersionDep ghcVer ghcRel = "ghc=" ++ display ghcVer ++ "-" ++ show ghcRel
 
