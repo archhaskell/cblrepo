cd0c094acdd8c1b5d8fb37b4ce49cffbaea5450b
Magnus Therning <magnus@therning.org>
2016-10-04 13:56:52 +0200

    Re-indent Upgrades.hs.
    Signed-off-by: Magnus Therning <magnus@therning.org>

--- src/Upgrades.hs
+++ src/Upgrades.hs
@@ -32,29 +32,31 @@ import Distribution.Version hiding (thisVersion)
 
 upgrades :: Command ()
 upgrades = do
-    db <- asks (dbFile . fst) >>= liftIO . readDb
-    aD <- asks $ appDir . fst
-    aCS <- asks $ idxStyle .optsCmd . fst
-    xrevsOnly <- asks $ xrevs . optsCmd . fst
-    cfg <- asks snd
-    availPkgsNVers <- liftIO $ buildPkgVersions <$> readIndexFile aD (getIndexFileName cfg)
-    let nonBasePkgs = filter (not . isBasePkg) db
-        pkgsNVers = map (pkgName &&& pkgVersion &&& pkgXRev) nonBasePkgs
-        filterFunc = if xrevsOnly
-            then (\ (p, (v, x)) -> maybe False (> x) (snd <$> thisVersion availPkgsNVers p v))
-            else (\ (p, vx) -> maybe False (> vx) (latestVersion availPkgsNVers p undefined))
-        outdated = filter filterFunc pkgsNVers
-        printer = if aCS
-            then printNewShort
-            else printUpgrades
-    if xrevsOnly
-      then liftIO $ mapM_ (printer thisVersion availPkgsNVers) outdated
-      else liftIO $ mapM_ (printer latestVersion availPkgsNVers) outdated
+  db <- asks (dbFile . fst) >>= liftIO . readDb
+  aD <- asks $ appDir . fst
+  aCS <- asks $ idxStyle .optsCmd . fst
+  xrevsOnly <- asks $ xrevs . optsCmd . fst
+  cfg <- asks snd
+  availPkgsNVers <- liftIO $ buildPkgVersions <$> readIndexFile aD (getIndexFileName cfg)
+  let nonBasePkgs = filter (not . isBasePkg) db
+      pkgsNVers = map (pkgName &&& pkgVersion &&& pkgXRev) nonBasePkgs
+      filterFunc = if xrevsOnly
+                   then (\ (p, (v, x)) -> maybe False (> x) (snd <$> thisVersion availPkgsNVers p v))
+                   else (\ (p, vx) -> maybe False (> vx) (latestVersion availPkgsNVers p undefined))
+      outdated = filter filterFunc pkgsNVers
+      printer = if aCS
+                then printNewShort
+                else printUpgrades
+  if xrevsOnly
+    then liftIO $ mapM_ (printer thisVersion availPkgsNVers) outdated
+    else liftIO $ mapM_ (printer latestVersion availPkgsNVers) outdated
 
-printUpgrades verFunc avail (pkgName, (pkgVer, pkgXRev)) = putStrLn $ pkgName ++ ": " ++ display pkgVer ++ ":x" ++ show pkgXRev ++ " (" ++ display lv ++ ":x" ++ show lx ++ ")"
-    where
-        (lv, lx) = fromJust $ verFunc avail pkgName pkgVer
+printUpgrades verFunc avail (pkgName, (pkgVer, pkgXRev)) = putStrLn $
+  pkgName ++ ": " ++ display pkgVer ++ ":x" ++ show pkgXRev ++ " (" ++ display lv ++ ":x" ++ show lx ++ ")"
+  where
+    (lv, lx) = fromJust $ verFunc avail pkgName pkgVer
 
-printNewShort verFunc avail (pkgName, (pkgVer, _)) = putStrLn $ pkgName ++ "," ++ display l
-    where
-        l = fst $ fromJust $ verFunc avail pkgName pkgVer
+printNewShort verFunc avail (pkgName, (pkgVer, _)) = putStrLn $
+  pkgName ++ "," ++ display l
+  where
+    l = fst $ fromJust $ verFunc avail pkgName pkgVer
